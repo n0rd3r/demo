5cd0ede4c115a7bf8c187b6c294f1eedad191f72
norder <jeff@norjekudo.com>
2016-08-14 20:51:42 -0400

    pass args to the controller

--- rest/src/main/java/rest/App.java
+++ rest/src/main/java/rest/App.java
@@ -2,11 +2,32 @@ package rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @SpringBootApplication
 public class App {
-
+    static Properties prop = new Properties();
+    static InputStream input = null;
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        String[] a = new String[1];
+        try {
+            input = new FileInputStream(".rest.properties");
+            prop.load(input);
+            a[0] = prop.getProperty("forecast.io.key");
+            SpringApplication.run(App.class, a);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- rest/src/main/java/rest/YoController.java
+++ rest/src/main/java/rest/YoController.java
@@ -4,15 +4,25 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
 
 @RestController
+@Component
 public class YoController {
 
     private static final String template = "Yo, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private String forecastIoKey = "";
+    @Autowired
+    public YoController(ApplicationArguments args) {
+        forecastIoKey = args.getSourceArgs()[0];
+    }
 
     @RequestMapping("/yo")
     public Yo greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        System.out.println("Forecast.io Key: " + forecastIoKey);
         return new Yo(counter.incrementAndGet(),
                             String.format(template, name));
     }
